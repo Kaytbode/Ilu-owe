8236023ee6bb32e74e46b5e6d34e6f19e1967156
Kayode <bkaoluborode@gmail.com>
2018-09-21 16:49:51 +0100

    fix: Modify event listener

--- js/main.js
+++ js/main.js
@@ -5,7 +5,11 @@ DBhelper.registerSW();
 let proverbInterval = window.setInterval(DBhelper.getProverb, 60000);
 
 // tweet proverb
-document.getElementById('tweet').addEventListener('click', DBhelper.postTweet);
+document.getElementById('tweet').addEventListener('click', ()=>{
+    DBhelper.postTweet();
+});
 
 // check if user is online, post proverb stored in database if any
-window.addEventListener('online', DBhelper.getProverbFromIDB);
+window.addEventListener('online', ()=>{
+    DBhelper.getProverbFromIDB();
+});
