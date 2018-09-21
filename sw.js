55346d2177d990cfa8dc530809f61f9bf46bb36a
Kayode <bkaoluborode@gmail.com>
2018-09-21 17:54:43 +0100

    fix: Google fonts cached properly

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'ilu-owe-v3';
+const staticCacheName = 'ilu-owe-v4';
 
 addEventListener('install', event=>{
     event.waitUntil(
@@ -12,9 +12,13 @@ addEventListener('install', event=>{
                 '/Ilu-owe/js/data.js',
                 '/Ilu-owe/js/idb.js',
                 '/Ilu-owe/js/dbHelper.js',
-                'https://fonts.googleapis.com/css?family=Kosugi|Raleway|Bad+Script|Inconsolata',
+                'https://fonts.gstatic.com/s/badscript/v6/6NUT8F6PJgbFWQn47_x7pOskzP9Ddt0.woff2',
+                'https://fonts.gstatic.com/s/inconsolata/v16/QldKNThLqRwH-OJ1UHjlKGlZ5qhExfHw.woff2',
+                'https://fonts.gstatic.com/s/kosugi/v1/pxiFyp4_v8FCjlI4Mbr8f1sHNv2pIig0ywDN1ZcQ_RTatRAh4ITLsBMU10F85RXF0VxEtYOtOBQj.98.woff2',
+                'https://fonts.gstatic.com/s/kosugi/v1/pxiFyp4_v8FCjlI4Mbr8f1sHNv2pIig0ywDN1ZcQ_RTatRAh4ITLsBMU10F85RXF0VxEtYOtOBQj.118.woff2',
+                'https://fonts.gstatic.com/s/kosugi/v1/pxiFyp4_v8FCjlI4Mbr8f1sHNv2pIig0ywDN1ZcQ_RTatRAh4ITLsBMU10F85RXF0VxEtYOtOBQj.119.woff2',
+                'https://fonts.gstatic.com/s/raleway/v12/1Ptug8zYS_SKggPNyC0IT4ttDfA.woff2',
                 'https://use.fontawesome.com/releases/v5.3.1/css/all.css'
-
             ]).then(()=> self.skipWaiting())
         })
     );
@@ -41,9 +45,9 @@ addEventListener('fetch', event=>{
             if(response) return response
             
             return fetch(event.request).then(response=>{
-               // if(response.status === 404){
-               //     return caches.match('/page404.html');
-               // }
+                if(response.status === 404){
+                    return caches.match('/page404.html');
+                }
                 return response;
             });
         })
