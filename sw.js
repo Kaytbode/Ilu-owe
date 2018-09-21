42fdc28a1bf55ecff09d5b7c5d16468a53dafb3f
Kayode <bkaoluborode@gmail.com>
2018-09-21 18:03:10 +0100

    fix: Cache google fonts

--- sw.js
+++ sw.js
@@ -12,13 +12,8 @@ addEventListener('install', event=>{
                 '/Ilu-owe/js/data.js',
                 '/Ilu-owe/js/idb.js',
                 '/Ilu-owe/js/dbHelper.js',
-                'https://fonts.gstatic.com/s/badscript/v6/6NUT8F6PJgbFWQn47_x7pOskzP9Ddt0.woff2',
-                'https://fonts.gstatic.com/s/inconsolata/v16/QldKNThLqRwH-OJ1UHjlKGlZ5qhExfHw.woff2',
-                'https://fonts.gstatic.com/s/kosugi/v1/pxiFyp4_v8FCjlI4Mbr8f1sHNv2pIig0ywDN1ZcQ_RTatRAh4ITLsBMU10F85RXF0VxEtYOtOBQj.98.woff2',
-                'https://fonts.gstatic.com/s/kosugi/v1/pxiFyp4_v8FCjlI4Mbr8f1sHNv2pIig0ywDN1ZcQ_RTatRAh4ITLsBMU10F85RXF0VxEtYOtOBQj.118.woff2',
-                'https://fonts.gstatic.com/s/kosugi/v1/pxiFyp4_v8FCjlI4Mbr8f1sHNv2pIig0ywDN1ZcQ_RTatRAh4ITLsBMU10F85RXF0VxEtYOtOBQj.119.woff2',
-                'https://fonts.gstatic.com/s/raleway/v12/1Ptug8zYS_SKggPNyC0IT4ttDfA.woff2',
-                'https://use.fontawesome.com/releases/v5.3.1/css/all.css'
+                'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+                'https://fonts.googleapis.com/css?family=Kosugi|Raleway|Bad+Script|Inconsolata'
             ]).then(()=> self.skipWaiting())
         })
     );
